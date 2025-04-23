1e60c747722a5fddbe7f8562506bfcabc48f1637
yuluo <yuluozy@outlook.com>
2025-04-23 19:34:11 +0800

    add IgnoredAllUsersEventContent

--- crates/ruma-events/src/ignored_all_users.rs
+++ crates/ruma-events/src/ignored_all_users.rs
@@ -5,18 +5,52 @@ use ruma_macros::EventContent;
 #[cfg_attr(not(feature = "unstable-exhaustive-types"), non_exhaustive)]
 #[ruma_event(type = "m.ignored_all_users", kind = GlobalAccountData)]
 pub struct IgnoredAllUsersEventContent {
-    pub enabled: String,
+    pub enabled: bool,
 }
 
 impl IgnoredAllUsersEventContent {
     pub fn new(enabled: bool) -> Self {
-        if enabled {
-            return Self {
-                enabled: "enable".to_string(),
-            }
-        }
         Self {
-            enabled: "disable".to_string(),
+            enabled
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use assert_matches2::assert_matches;
+    use serde_json::{from_value as from_json_value, json, to_value as to_json_value};
+
+    use super::IgnoredAllUsersEventContent;
+    use crate::AnyGlobalAccountDataEvent;
+
+    #[test]
+    fn serialization() {
+        let ignored_all_users_event_content = IgnoredAllUsersEventContent::new(true);
+
+        let json = json!({
+            "enabled": true,
+        });
+
+        assert_eq!(to_json_value(ignored_all_users_event_content).unwrap(), json);
+    }
+
+    #[test]
+    fn deserialization() {
+        let json = json!({
+            "content": {
+               "enabled": true
+            },
+            "type": "m.ignored_all_users"
+        });
+
+        assert_matches!(
+            from_json_value::<AnyGlobalAccountDataEvent>(json),
+            Ok(AnyGlobalAccountDataEvent::IgnoredAllUsers(ev))
+        );
+        assert_eq!(
+            ev.content.enabled,
+            true
+        );
     }
 }
